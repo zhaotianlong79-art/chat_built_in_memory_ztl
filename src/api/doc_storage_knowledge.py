ca7adaa3b19deba031dc89123372934fa5f6ecca
zhaokunming <718831941@qq.com>
2025-12-09 17:50:27 +0800

    feat: Add doc storage knowledge API endpoints

---
+++ src/api/doc_storage_knowledge.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from fastapi import APIRouter, UploadFile, File
+
+from src.schemas.doc_storage_knowledge_schemas import DocKnowledgeBase
+
+router = APIRouter()
+
+
+@router.post("/doc2knowledge_base")
+async def doc2knowledge_base(params: DocKnowledgeBase):
+    pass
+
+
+@router.post("/doc2knowledge_base")
+async def doc2knowledge_base(files: List[UploadFile] = File(...)):
+    pass
